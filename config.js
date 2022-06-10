cc9ad4e9596ead054cd3ebc4a21b68ae8a64f50c
Damien <damien.herrero@epitech.eu>
2022-06-10 17:17:49 +0200

    change from createConnection to createPool

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 // require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
 
-const con = mysql.createConnection({
+// const con = mysql.createConnection({
+const con = mysql.createPool({
    host: process.env.HOST,
    user: process.env.MYUSER,
    password: process.env.PASSWORD,
